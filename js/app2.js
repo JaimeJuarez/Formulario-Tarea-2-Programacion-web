74285050ebc328fe1d1d36f572d84e1c2989a531
Jaime <jjuarez17@ucol.mx>
2021-09-19 17:26:00 -0500

    Update 09-19-2021

--- js/app2.js
+++ js/app2.js
@@ -1,5 +1,5 @@
-var inventario = [];
-var demo = document.getElementById('demo');
+var INVENTARIO = [];
+const DEMO = document.getElementById('demo');
 window.onload = eventos;
 
 function eventos() {
@@ -15,37 +15,60 @@ function agregarDato() {
         Username: document.getElementById('exampleInput2').value,
         Email: document.getElementById('exampleInput3').value,
         Phone: document.getElementById('exampleInput4').value,
-        Website: document.getElementById('exampleInput5').value
+        Website: document.getElementById('exampleInput5').value,
+        Street: document.getElementById("exampleInput6").value,
+        City: document.getElementById("exampleInput7").value,
+        Zipcode: document.getElementById("exampleInput8").value,
+        Company: document.getElementById("exampleInput9").value
     }
-    inventario.push(newInventario);
+    INVENTARIO.push(newInventario);
     newInventario = {
         Id: document.getElementById('exampleInput0').value = '',
         Name: document.getElementById('exampleInput1').value = '',
         Username: document.getElementById('exampleInput2').value = '',
         Email: document.getElementById('exampleInput3').value = '',
         Phone: document.getElementById('exampleInput4').value = '',
-        Website: document.getElementById('exampleInput5').value = ''
+        Website: document.getElementById('exampleInput5').value = '',
+        Street: document.getElementById("exampleInput6").value = '',
+        City: document.getElementById("exampleInput7").value = '',
+        Zipcode: document.getElementById("exampleInput8").value = '',
+        Company: document.getElementById("exampleInput9").value = ''
     }
-    console.log(inventario);
+    console.log(INVENTARIO);
 }
 
 function mostarDatos() {
     var DatosUsuario = "" + "<br>";
-    for (let i = 0; i < inventario.length; i++) {
-        DatosUsuario += "<br>" + "Id de usuario: " + inventario[i].Id + "<br>" + "Nombre: " + inventario[i].Name + "<br>" + "Nombre de Usuario: " +
-            inventario[i].Username + "<br>" + "Email de usuario: " + inventario[i].Email + "<br>" + "Telefono de Usuario: " + inventario[i].Phone +
-            "<br>" + " Sitio Web de usuario: " + inventario[i].Website + "<br>";
+    for (let i = 0; i < INVENTARIO.length; i++) {
+        DatosUsuario += "<br>" + "<b>" + "Id de usuario: " + "</b>" + INVENTARIO[i].Id + "<br>" + "<b>" + "Nombre: " + "</b>" + INVENTARIO[i].Name + "<br>" + "<b>" + "Nombre de Usuario: " +
+            "</b>" + INVENTARIO[i].Username + "<br>" + "<b>" + "Email de usuario: " + "</b>" + INVENTARIO[i].Email + "<br>" + "<b>" + "Telefono de Usuario: " + "</b>" + INVENTARIO[i].Phone +
+            "<br>" + "<b>" + "Sitio Web de usuario: " + "</b>" + INVENTARIO[i].Website + "<br>" + "<b>" + "Calle del usuario: " + "</b>" + INVENTARIO[i].Street + "<br>" + "<b>" + "Ciudad del usuario: " + "</b>" + INVENTARIO[i].City + "<br>" +
+            "<b>" + "Codigo postal del usuario: " + "</b>" + INVENTARIO[i].Zipcode + "<br>" + "<b>" + "Compañia del usuario: " + "</b>" + INVENTARIO[i].Company + "<br>";
     }
-    demo.innerHTML = DatosUsuario + "<br>";
-    console.log(demo);
+    DEMO.innerHTML = DatosUsuario + "<br>";
+    console.log(DEMO);
 }
 
-function borrardatos() {
-    for (let i = 0; i < inventario.length; i++) {
-        var removed = inventario.splice(i);
-        // if (inventario[0] == document.getElementById('exampleInput0').value) {
-        // }
+// function borrardatos(Id) {
+//     if (Id == document.getElementById('exampleInput0').value) {
+//         for (let i = 0; i < inventario.length; i++) {
+//             var removed = inventario.splice(i);
 
-    }
-    return removed;
+
+//         }
+//         return removed;
+//     }
+
+// }
+
+function borrardatos(id) {
+    let results = INVENTARIO.filter(item => {
+        return item.Id != id;
+    })
+    INVENTARIO = results;
+    console.log(results);
+    console.log(id);
 }
+//Ciclo para encontrar el id del elemento, y devolver indice.
+
+//Hacer el slice del array.
